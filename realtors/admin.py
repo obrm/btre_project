f7fd2487ff32a702dbd1a90afd1a72f358047149
Ori Baram <obrm770@gmail.com>
2022-10-02 14:43:34 +0300

    admin

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Realtor
 
-# Register your models here.
+class RealtorAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'is_mvp', 'email', 'hire_date')
+  list_display_links = ('id', 'name')
+  list_editable =('is_mvp',)
+  search_fields = ('name',)
+  list_per_page = 25
 
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
