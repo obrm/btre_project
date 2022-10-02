4c1792ba846ed216ec657230cd91bef14e90e5f3
Ori Baram <obrm770@gmail.com>
2022-10-02 14:18:38 +0300

    admin

--- btre/settings.py
+++ btre/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 's2afc350',
+        'HOST': 'localhost'
     }
 }
 
@@ -127,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Listing
+from .models import Realtor
 
 # Register your models here.
 
-admin.site.register(Listing)
+admin.site.register(Realtor)
