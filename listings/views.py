acae212fdc10201c6fe19f042da72ac3f4cfca5f
Ori Baram <obrm770@gmail.com>
2022-10-23 13:32:09 +0300

    listings

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import Listing
 
 def index(request):
-  return render(request, 'listings/listings.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-  return render(request, 'listings/listing.html')
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+
+  context = {
+    'listings': paged_listings
+  }
+
+  return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+  listing = get_object_or_404(Listing, pk=listing_id)
+
+  context = {
+    'listing': listing
+  }
+
+  return render(request, 'listings/listing.html', context)
 
 def search(request):
   return render(request, 'listings/search.html')
