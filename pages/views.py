73dce21e15d87c2d17229616f614caed5647801a
Ori Baram <obrm770@gmail.com>
2022-10-24 10:13:18 +0300

    search

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 
@@ -9,6 +10,9 @@ def index(request):
 
   context ={
     'listings': listings,
+    'state_choices': state_choices,
+    'price_choices': price_choices,
+    'bedroom_choices': bedroom_choices,
   }
 
   return render(request, 'pages/index.html', context)
